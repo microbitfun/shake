bd3b1d9c7aeb6a54bc3e7435ae96ad6a74a0e8ee
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-02 14:16:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onGesture(Gesture.SixG, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Happy)
+})
